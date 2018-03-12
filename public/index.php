4eee0eb4a191e8b697c4f2ef8d053248f0120c48
nathalie4499 <nathalie.montois@outlook.com>
2018-03-12 10:27:50 +0100

    dev branch

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<title>My Cats Web App</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+	<h1>Cats Database Application</h1>
+
+	<ul>
+		<li><a href="create.php"><strong>Create</strong></a> - add a new cat</li>
+		<li><a href="find.php"><strong>Find</strong></a> - find a cat</li>
+	</ul>
+
+</body>
+</html>
